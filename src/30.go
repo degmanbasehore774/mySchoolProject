189ee7f098652ffe7d31d82e0f3d312219fd9c61
degmanbasehore774 <degmanbasehore774@gmail.com>
2025-04-11 12:08:07 +0000

    Add 30.go

---
+++ src/30.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Example of simple Go code with comments
+    x := 5
+    y := 10
+    if x < y {
+        fmt.Printf("x is less than y\n")
+    } else {
+        fmt.Printf("x is not less than y\n")
+    }
+}
